fb31ce62b911dfd7e6f57b6e28ea39ccd7b4f107
wmassoudy <massoudy@smala12.de>
2020-12-08 18:51:12 +0100

    first commit

---
+++ src/main/java/com/javaBrain/git/GitTuTorialFromAutomationTcheApplication.java
@@ -0,0 +1,13 @@
+package com.javaBrain.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTuTorialFromAutomationTcheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTuTorialFromAutomationTcheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaBrain/git/GitTuTorialFromAutomationTcheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaBrain.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTuTorialFromAutomationTcheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
